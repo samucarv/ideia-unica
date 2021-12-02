cbac74677d351bcafca820fecbae67f37cf1e1aa
Samuel Bueno <samuelbueno@Samuels-MacBook-Pro.local>
2021-12-02 13:01:42 -0300

    rifa

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,236 @@ function Home(){
       <td>Myrna</td>
     </tr>
     
+    <tr>
+      <th scope="row">11</th>
+      <td>Myrna</td>
+    </tr>
+
+    <tr>
+      <th scope="row">12</th>
+      <td>Adna</td>
+    </tr>
+
+    <tr>
+      <th scope="row">13</th>
+      <td>Myrna</td>
+    </tr>
+
+    <tr>
+      <th scope="row">14</th>
+      <td>Myrna</td>
+    </tr>
+
+    <tr>
+      <th scope="row">15</th>
+      <td>Myrna</td>
+    </tr>
+
+    <tr>
+      <th scope="row">16</th>
+      <td>Myrna</td>
+    </tr>
+
+    <tr>
+      <th scope="row">17</th>
+      <td>Myrna</td>
+    </tr>
+
+    <tr>
+      <th scope="row">18</th>
+      <td>Myrna</td>
+    </tr>
+
+    <tr>
+      <th scope="row">19</th>
+      <td>Myrna</td>
+    </tr>
     
+    <tr>
+      <th scope="row">20</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">21</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">22</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">23</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">24</th>
+      <td>Myrna</td>
+    </tr>
+
+    <tr>
+      <th scope="row">25</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">26</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">27</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">28</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">29</th>
+      <td></td>
+    </tr>
+    
+    <tr>
+      <th scope="row">30</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">31</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">32</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">33</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">34</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">35</th>
+      <td>Myrna</td>
+    </tr>
+
+    <tr>
+      <th scope="row">36</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">37</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">38</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">39</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">40</th>
+      <td></td>
+    </tr>
+    
+    <tr>
+      <th scope="row">41</th>
+      <td></td>
+    </tr>
+
+
+
+
+    <tr>
+      <th scope="row">42</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">43</th>
+      <td></td>
+    </tr>
+    
+    <tr>
+      <th scope="row">44</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">45</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">46</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">47</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">48</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">49</th>
+      <td>Myrna</td>
+    </tr>
+
+    <tr>
+      <th scope="row">50</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">51</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">52</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">53</th>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">54</th>
+      <td></td>
+    </tr>
+    
+    <tr>
+      <th scope="row">55</th>
+      <td></td>
+    </tr>
+
+
+
   </tbody>
 </table>
 
