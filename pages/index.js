701dd99469ccf48bee97a56101779df06bbd0586
Samuel Bueno <samuelbueno@Samuels-MacBook-Pro.local>
2021-12-02 13:21:53 -0300

    rifa

--- pages/index.js
+++ pages/index.js
@@ -13,212 +13,254 @@ function Home(){
   <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">Nome</th>     
+      <th scope="col">Nome</th>    
+      <th scope="col">Pago</th> 
     </tr>
   </thead>
   <tbody>
     <tr>
       <th scope="row">01</th>
       <td>Myrna</td>
+      <td>Sim</td>
     </tr>
 
     <tr>
       <th scope="row">02</th>
       <td>Adna</td>
+      <td>Sim</td>
     </tr>
 
     <tr>
       <th scope="row">03</th>
       <td>Myrna</td>
+      <td>Sim</td>
     </tr>
 
     <tr>
       <th scope="row">04</th>
       <td>Myrna</td>
+      <td>Sim</td>
     </tr>
 
     <tr>
       <th scope="row">05</th>
       <td>Myrna</td>
+      <td>Sim</td>
     </tr>
 
     <tr>
       <th scope="row">06</th>
       <td>Myrna</td>
+      <td>Sim</td>
     </tr>
 
     <tr>
       <th scope="row">07</th>
       <td>Fábio Soares</td>
+      <td>Sim</td>
     </tr>
 
     <tr>
       <th scope="row">08</th>
       <td>Isabella</td>
+      <td>Sim</td>
     </tr>
 
     <tr>
       <th scope="row">09</th>
       <td>Myrna</td>
+      <td>Sim</td>
     </tr>
     
     <tr>
       <th scope="row">10</th>
       <td>Myrna</td>
+      <td>Sim</td>
     </tr>
     
     <tr>
       <th scope="row">11</th>
       <td>Myrna</td>
+      <td>Sim</td>
     </tr>
 
     <tr>
       <th scope="row">12</th>
       <td>Myrna</td>
+      <td>Sim</td>
     </tr>
 
     <tr>
       <th scope="row">13</th>
       <td>Myrna</td>
+      <td>Sim</td>
     </tr>
 
     <tr>
       <th scope="row">14</th>
       <td>Lidiane (Ro) </td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">15</th>
       <td>Dr. Moises</td>
+      <td>Sim</td>
     </tr>
 
     <tr>
       <th scope="row">16</th>
       <td>Andressa</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">17</th>
       <td>Dr. Moises</td>
+      <td>Sim</td>
     </tr>
 
     <tr>
       <th scope="row">18</th>
       <td>Maria Elisete</td>
+      <td>Nao</td>
     </tr>
 
     <tr>
       <th scope="row">19</th>
       <td>Dona Antonia</td>
+      <td>Nao</td>
     </tr>
     
     <tr>
       <th scope="row">20</th>
-      <td>Thaiane</td>
+      <td>Thaiany</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">21</th>
       <td>Lelia (Ro)</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">22</th>
       <td>Roberta</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">23</th>
       <td>Dany</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">24</th>
       <td>Paulo Henrique TV</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">25</th>
       <td>Tyssiane</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">26</th>
       <td>Adna</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">27</th>
       <td>Tulio</td>
+      <td>Sim</td>
     </tr>
 
     <tr>
       <th scope="row">28</th>
       <td>Thaiany</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">29</th>
       <td>Dany</td>
+      <td></td>
     </tr>
     
     <tr>
       <th scope="row">30</th>
       <td>Dama</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">31</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">32</th>
       <td>Lucijaine</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">33</th>
       <td>Dani Rezende</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">34</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">35</th>
-      <td>Myrna</td>
+      <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">36</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">37</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">38</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">39</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">40</th>
       <td></td>
+      <td></td>
     </tr>
     
     <tr>
       <th scope="row">41</th>
+      <td></td>
       <td></td>
     </tr>
 
@@ -228,70 +270,84 @@ function Home(){
     <tr>
       <th scope="row">42</th>
       <td>Tyssiane</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">43</th>
+      <td></td>
       <td></td>
     </tr>
     
     <tr>
       <th scope="row">44</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">45</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">46</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">47</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">48</th>
       <td>Lucijaine</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">49</th>
-      <td>Myrna</td>
+      <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">50</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">51</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">52</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">53</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">54</th>
       <td></td>
+      <td></td>
     </tr>
     
     <tr>
       <th scope="row">55</th>
+      <td></td>
       <td></td>
     </tr>
 
@@ -301,70 +357,84 @@ function Home(){
     <tr>
       <th scope="row">56</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">57</th>
+      <td></td>
       <td></td>
     </tr>
     
     <tr>
       <th scope="row">58</th>
       <td>Dani Rezende</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">59</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">60</th>
       <td>Dany</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">61</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">62</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">63</th>
       <td>Myrna</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">64</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">65</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">66</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">67</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">68</th>
       <td></td>
+      <td></td>
     </tr>
     
     <tr>
       <th scope="row">69</th>
+      <td></td>
       <td></td>
     </tr>
 
@@ -374,71 +444,85 @@ function Home(){
     <tr>
       <th scope="row">70</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">71</th>
+      <td></td>
       <td></td>
     </tr>
     
     <tr>
       <th scope="row">72</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">73</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">74</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">75</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">76</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">77</th>
       <td>Myrna</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">78</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">79</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">80</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">81</th>
       <td>Rosangela</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">82</th>
+      <td></td>
       <td></td>
     </tr>
     
     <tr>
       <th scope="row">83</th>
       <td>Dany</td>
+      <td></td>
     </tr>
 
 
@@ -447,45 +531,54 @@ function Home(){
     <tr>
       <th scope="row">84</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">85</th>
       <td>Dany</td>
+      <td></td>
     </tr>
     
     <tr>
       <th scope="row">86</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">87</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">88</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">89</th>
       <td>Hiago (Ro)</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">90</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">91</th>
-      <td>Myrna</td>
+      <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">92</th>
+      <td></td>
       <td></td>
     </tr>
 
@@ -497,34 +590,41 @@ function Home(){
     <tr>
       <th scope="row">94</th>
       <td></td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">95</th>
+      <td></td>
       <td></td>
     </tr>
 
     <tr>
       <th scope="row">96</th>
       <td></td>
+      <td></td>
     </tr>
     
     <tr>
       <th scope="row">97</th>
       <td></td>
+      <td></td>
     </tr>
     <tr>
       <th scope="row">98</th>
+      <td></td>
       <td></td>
     </tr>
     
     <tr>
       <th scope="row">99</th>
       <td>Marcus Vinicius TV</td>
+      <td></td>
     </tr>
 
     <tr>
       <th scope="row">100</th>
+      <td></td>
       <td></td>
     </tr>
 
