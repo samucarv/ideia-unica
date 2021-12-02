26420a3c02c13f7b0ba10821d77f3fb0e660bd6d
Samuel Bueno <samuelbueno@Samuels-MacBook-Pro.local>
2021-12-02 12:22:11 -0300

    rifa

--- pages/index.js
+++ pages/index.js
@@ -9,22 +9,38 @@ function Home(){
     <font color="white"><center> RIFA - CHURRASQUEIRA ELÉTRICA</center>  </font>  
     </nav>
 
-    <table>
-        <tr>
-            <td>NUMERO</td>
-            <td>NOME</td>    
-        </tr>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
 
-        <tr>
-            <td>01</td>
-            <td>SAMUEL</td>    
-        </tr>
-        </table>
-        
 
-
-
-
+   
     
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
